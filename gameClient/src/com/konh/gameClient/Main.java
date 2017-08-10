2e885f9857b970fc056e12f899732338dc8eeab6
KonH <konh@yandex.ru>
2017-08-10 18:10:05 +0700

    Spaces to tabs;

--- gameClient/src/com/konh/gameClient/Main.java
+++ gameClient/src/com/konh/gameClient/Main.java
@@ -2,7 +2,7 @@ package com.konh.gameClient;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("It is game client!");
-    }
+	public static void main(String[] args) {
+		System.out.println("It is game client!");
+	}
 }
